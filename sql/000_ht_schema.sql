38b61d0554e426cfcf301a0edb746ccf66137626
moseshll <moseshll@umich.edu>
2025-08-20 16:05:47 -0400

    Add feed tables for testing HT-519 and HT-520

--- sql/000_ht_schema.sql
+++ sql/000_ht_schema.sql
@@ -344,6 +344,200 @@ CREATE TABLE `holdings_htitem_htmember` (
   KEY `cluster_id` (`cluster_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+-----------------
+-- feed Tables --
+-- feed_last_error is a view, so it is not captured here
+-----------------
+
+DROP TABLE IF EXISTS `feed_audit_detail`;
+CREATE TABLE `feed_audit_detail` (
+  `namespace` varchar(10) NOT NULL,
+  `id` varchar(30) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  `detail` tinytext DEFAULT NULL,
+  `time` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT current_timestamp(),
+  KEY `fs_log_status_objid_idx` (`namespace`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_audit`;
+CREATE TABLE `feed_audit` (
+  `namespace` varchar(10) NOT NULL,
+  `id` varchar(30) NOT NULL,
+  `sdr_partition` tinyint(4) DEFAULT NULL,
+  `zip_size` bigint(20) DEFAULT NULL,
+  `image_size` bigint(20) DEFAULT NULL,
+  `zip_date` datetime /* mariadb-5.3 */ DEFAULT NULL,
+  `mets_size` bigint(20) DEFAULT NULL,
+  `mets_date` datetime /* mariadb-5.3 */ DEFAULT NULL,
+  `page_count` int(11) DEFAULT NULL,
+  `lastchecked` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT current_timestamp(),
+  `lastmd5check` timestamp /* mariadb-5.3 */ NULL DEFAULT NULL,
+  `md5check_ok` tinyint(1) DEFAULT NULL,
+  `is_tombstoned` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`namespace`,`id`),
+  KEY `feed_audit_zip_date_idx` (`zip_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_backups`;
+CREATE TABLE `feed_backups` (
+  `namespace` varchar(10) NOT NULL,
+  `id` varchar(32) NOT NULL,
+  `path` text DEFAULT NULL,
+  `version` varchar(16) NOT NULL,
+  `storage_name` varchar(32) NOT NULL,
+  `zip_size` bigint(20) DEFAULT NULL,
+  `mets_size` bigint(20) DEFAULT NULL,
+  `saved_md5sum` char(32) DEFAULT NULL,
+  `lastchecked` timestamp NOT NULL DEFAULT current_timestamp(),
+  `lastmd5check` timestamp NULL DEFAULT NULL,
+  `restore_request` timestamp NULL DEFAULT NULL,
+  `md5check_ok` tinyint(1) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT NULL,
+  KEY `feed_backups_objid` (`namespace`,`id`),
+  KEY `feed_backups_version` (`version`),
+  KEY `feed_backups_restore_request_idx` (`restore_request`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_grin`;
+CREATE TABLE `feed_grin` (
+  `namespace` varchar(10) NOT NULL DEFAULT '',
+  `id` varchar(30) NOT NULL DEFAULT '',
+  `checkin_date` datetime DEFAULT NULL,
+  `state` varchar(30) DEFAULT NULL,
+  `viewability` varchar(15) DEFAULT NULL,
+  `conditions` varchar(30) DEFAULT NULL,
+  `scannable` varchar(5) DEFAULT NULL,
+  `opted_out` varchar(5) DEFAULT NULL,
+  `tagging` varchar(5) DEFAULT NULL,
+  `audit` varchar(30) DEFAULT NULL,
+  `material_error` varchar(4) DEFAULT NULL,
+  `overall_error` varchar(4) DEFAULT NULL,
+  `scan_date` datetime DEFAULT NULL,
+  `process_date` datetime DEFAULT NULL,
+  `analyze_date` datetime DEFAULT NULL,
+  `convert_date` datetime DEFAULT NULL,
+  `allow_date` datetime DEFAULT NULL,
+  `viewability_date` datetime DEFAULT NULL,
+  `src_lib_bibkey` varchar(25) DEFAULT NULL,
+  `rubbish` varchar(5) DEFAULT NULL,
+  `dl_date` datetime DEFAULT NULL,
+  `claimed` varchar(5) DEFAULT NULL,
+  `ocr_gtd` varchar(4) DEFAULT NULL,
+  `ocr_analysis` varchar(4) DEFAULT NULL,
+  `digitization_method` varchar(32) DEFAULT NULL,
+  `ocr_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`namespace`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_ia_arkid`;
+CREATE TABLE `feed_ia_arkid` (
+  `ia_id` varchar(255) NOT NULL DEFAULT '',
+  `namespace` varchar(32) DEFAULT NULL,
+  `arkid` varchar(32) DEFAULT NULL,
+  `resolution` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`ia_id`),
+  KEY `arkid` (`arkid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_log`;
+CREATE TABLE `feed_log` (
+  `level` varchar(5) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `namespace` varchar(8) DEFAULT NULL,
+  `id` varchar(32) DEFAULT NULL,
+  `operation` varchar(32) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `file` text DEFAULT NULL,
+  `field` text DEFAULT NULL,
+  `actual` text DEFAULT NULL,
+  `expected` text DEFAULT NULL,
+  `detail` text DEFAULT NULL,
+  `stage` varchar(255) DEFAULT NULL,
+  KEY `log_objid_idx` (`namespace`,`id`),
+  KEY `log_timestamp_idx` (`timestamp`),
+  KEY `log_stage_idx` (`stage`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+DROP TABLE IF EXISTS `feed_premis_events`;
+CREATE TABLE `feed_premis_events` (
+  `namespace` varchar(8) NOT NULL DEFAULT '',
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `eventtype_id` varchar(64) NOT NULL DEFAULT '',
+  `date` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `outcome` text DEFAULT NULL,
+  `eventid` char(36) DEFAULT NULL,
+  `custom_xml` mediumtext DEFAULT NULL,
+  PRIMARY KEY (`namespace`,`id`,`eventtype_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_queue_disallow`;
+CREATE TABLE `feed_queue_disallow` (
+  `namespace` varchar(8) NOT NULL DEFAULT '',
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `note` text DEFAULT NULL,
+  `time` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`namespace`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_queue_done`;
+CREATE TABLE `feed_queue_done` (
+  `pkg_type` varchar(32) DEFAULT NULL,
+  `namespace` varchar(8) NOT NULL DEFAULT '',
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `update_stamp` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_added` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`namespace`,`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_queue`;
+CREATE TABLE `feed_queue` (
+  `pkg_type` varchar(32) DEFAULT NULL,
+  `namespace` varchar(8) NOT NULL DEFAULT '',
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `status` varchar(20) NOT NULL DEFAULT 'ready',
+  `reset_status` varchar(20) DEFAULT NULL,
+  `update_stamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `date_added` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `node` varchar(255) DEFAULT NULL,
+  `failure_count` int(11) NOT NULL DEFAULT 0,
+  `priority` int(11) DEFAULT NULL,
+  PRIMARY KEY (`namespace`,`id`),
+  KEY `queue_status_pkg_type_idx` (`status`,`pkg_type`),
+  KEY `queue_priority_idx` (`priority`,`date_added`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_watched_items`;
+CREATE TABLE `feed_watched_items` (
+  `namespace` varchar(8) DEFAULT NULL,
+  `id` varchar(32) DEFAULT NULL,
+  `request_date` timestamp /* mariadb-5.3 */ NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ready` tinyint(1) DEFAULT NULL,
+  `issue_key` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+DROP TABLE IF EXISTS `feed_zephir_items`;
+CREATE TABLE `feed_zephir_items` (
+  `namespace` varchar(5) NOT NULL,
+  `id` varchar(32) NOT NULL,
+  `collection` varchar(32) NOT NULL,
+  `digitization_source` varchar(32) NOT NULL,
+  `returned` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`namespace`,`id`),
+  KEY `collection` (`collection`,`digitization_source`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 
 GRANT SELECT ON `ht`.* TO 'mdp-lib'@'%' IDENTIFIED BY 'mdp-lib';
 GRANT ALL ON `ht`.* TO 'mdp-admin'@'%' IDENTIFIED BY 'mdp-admin';
